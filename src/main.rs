c968c6563216972d75bbb71d64e2f56b1ba82896
Eric Thomas <eric@ericthomas.ca>
2022-11-29 22:47:47 -0330

    remove impl to fix instantiation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use chrono::{DateTime, Utc};
 use uuid::Uuid;
 
-#[derive(Debug)]
+#[derive(Default, Debug)]
 struct Event {
     id: Uuid,
     performer: String,
@@ -11,28 +11,15 @@ struct Event {
     tickets_sold: u32,
 }
 
-impl Event {
-    pub fn new(
-        id: Uuid,
-        performer: String,
-        date: DateTime<Utc>,
-        location: String,
-        tickets_available: u32,
-        tickets_sold: u32,
-    ) -> Event {
-        Event {
-            id,
-            performer,
-            date,
-            location,
-            tickets_available,
-            tickets_sold,
-        }
-    }
-}
-
 fn main() {
-    let event: Event = Event<Event as Trait>::new{id: Uuid::new_v4(), performer: String::from("Tool"), date: chrono::offset::Utc::now(), location: String::from(""), ticket_available: 17_000, tickets_sold: 2500};
-  
+    let event = Event {
+        id: Uuid::new_v4(),
+        performer: String::from("Tool"),
+        date: chrono::offset::Utc::now(),
+        location: String::from("St. John's, NL"),
+        tickets_available: 17_000,
+        tickets_sold: 2500,
+    };
+
     println!("{:?}", event);
 }
